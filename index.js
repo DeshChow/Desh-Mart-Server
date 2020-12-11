fa7b071bf360902452d401d221860e7f0e12dfca
DeshChow <deshchow001@gmail.com>
2020-12-11 21:10:18 +0600

    6th day work done

--- index.js
+++ index.js
@@ -27,132 +27,171 @@ app.get('/', (req, res) => {
 
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 client.connect(err => {
-  const productCollection = client.db("DeshMart").collection("womensdressProducts");
- 
-
-      app.post('/addProducts',(req,res)=>
-      {
-          console.log(req.body);
-          console.log(req.files.file);
-        
-
-          const file = req.files.file;
-          const name = req.body.name;
-          const price= req.body.price;
-          const brand=req.body.brand;
-          const color=req.body.color;
-          const madeIn=req.body.madeIn;
-          const quantity=req.body.quantity;
-          const itemDetails=req.body.itemDetails;
-          const newImg = file.data;
-          const encImg = newImg.toString('base64');
-   
-          var image = {
-              contentType: file.mimetype,
-              size: file.size,
-              img: Buffer.from(encImg, 'base64')
-          };
-
-          console.log(1,req.body.category);
-   
-     client.db("DeshMart").collection(req.body.category).insertOne({ name,brand,color,madeIn, quantity,price,itemDetails,image })
-              .then(result => {
-                  res.send(result.insertedCount > 0);
-              })
-      });
-
-     /* app.get('/womensdressProducts',(req,res)=>
-      {
-             productCollection.find({})
-             .toArray((err,documents)=>
-             {
-                 res.send(documents);
-             }) 
-      })*/
-
-      app.get('/products/:key',(req,res)=>
-      {
-          const Collection=req.params.key;
-
-          console.log(Collection);
-
-          client.db("DeshMart").collection(Collection).find({})
-          .toArray((err,documents)=>
-          {
-              res.send(documents);
-          }) 
-      })
-
-      app.get('/products/:key',(req,res)=>
-      {
-          const Collection=req.params.key;
-
-          console.log(Collection);
-
-          client.db("DeshMart").collection(Collection).find({})
-          .toArray((err,documents)=>
-          {
-              res.send(documents);
-          }) 
-      })
-
-      app.get('/product/:_id/:category',(req,res)=>
-      {
-       
-                 const category=req.params.category;
-
-                 const _id=req.params._id;
-
-                 const ID=ObjectID(_id);
-
-                 console.log(typeof(ID),ID);
+    const productCollection = client.db("DeshMart").collection("womensdressProducts");
 
 
-        client.db("DeshMart").collection(category).find({_id:ID})
-        .toArray((err,documents)=>{
-
-            console.log(documents[0]);
-            res.send(documents[0]);
-        })
-        
+    app.post('/addProducts', (req, res) => {
+        console.log(req.body);
+        console.log(req.files.file);
 
 
+        const file = req.files.file;
+        const name = req.body.name;
+        const price = req.body.price;
+        const brand = req.body.brand;
+        const color = req.body.color;
+        const madeIn = req.body.madeIn;
+        const quantity = req.body.quantity;
+        const itemDetails = req.body.itemDetails;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
 
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
 
+        console.log(1, req.body.category);
 
-
-
-
-      })
-
-      app.delete('/delete/:id/:category',(req,res)=>{
-
-        client.db("DeshMart").collection(req.params.category).deleteOne({_id:ObjectID(req.params.id)})
-        .then(result => {
-            res.send(result.deletedCount > 0);
-        })
-      })
-
-      app.patch('/updateProduct',(req,res)=>
-      {
-        client.db("DeshMart").collection(req.body.category).updateOne({_id:ObjectID(req.body.id)},
-        {
-            $set:{price:req.body.price}
-        })
-        .then(result=>
-            {
-                res.send(result.modifiedCount>0);
+        client.db("DeshMart").collection(req.body.category).insertOne({ name, brand, color, madeIn, quantity, price, itemDetails, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
             })
+    });
+
+    /* app.get('/womensdressProducts',(req,res)=>
+     {
+            productCollection.find({})
+            .toArray((err,documents)=>
+            {
+                res.send(documents);
+            }) 
+     })*/
+
+    app.get('/products/:key', (req, res) => {
+        const Collection = req.params.key;
+
+        console.log(Collection);
+
+        client.db("DeshMart").collection(Collection).find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/products/:key', (req, res) => {
+        const Collection = req.params.key;
+
+        console.log(Collection);
+
+        client.db("DeshMart").collection(Collection).find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/product/:_id/:category', (req, res) => {
+
+        const category = req.params.category;
+
+        const _id = req.params._id;
+
+        const ID = ObjectID(_id);
+
+        console.log(typeof (ID), ID);
+
+
+        client.db("DeshMart").collection(category).find({ _id: ID })
+            .toArray((err, documents) => {
+
+                console.log(documents[0]);
+                res.send(documents[0]);
+            })
+
+
+
+
+
+
+
+
+
+    })
+
+    app.delete('/delete/:id/:category', (req, res) => {
+
+        client.db("DeshMart").collection(req.params.category).deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+    app.patch('/updateProduct', (req, res) => {
+        client.db("DeshMart").collection(req.body.category).updateOne({ _id: ObjectID(req.body.id) },
+            {
+                $set: { price: req.body.price }
+            })
+            .then(result => {
+                res.send(result.modifiedCount > 0);
+            })
+    })
+
+    app.post('/orderSave', (req, res) => {
+        console.log(req.body);
+
+        client.db("DeshMart").collection('orders').insertOne(req.body)
+            .then(result => {
+                client.db("DeshMart").collection('orderHistory').insertOne(req.body)
+                    .then(result => {
+                        res.send(result.insertedCount > 0);
+                    })
+            })
+
+
+    })
+
+    app.get('/orders', (req, res) => {
+
+
+          //console.log('hamaisi');
+        client.db("DeshMart").collection('orders').find({})
+        .toArray((err, documents) => {
+
+           // console.log((documents));
+            res.send(documents);
+        })
+
+    })
+    app.get('/orderHistory', (req, res) => {
+
+
+      //  console.log('hamaisi');
+      client.db("DeshMart").collection('orderHistory').find({})
+      .toArray((err, documents) => {
+
+         // console.log((documents));
+          res.send(documents);
       })
 
+      app.delete('/deleteOrder/:collection/:_id', (req, res) => {
 
-    
+        client.db("DeshMart").collection(req.params.collection).deleteOne({ _id: ObjectID(req.params._id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
 
-      console.log('db connected');
+  })
+
+
+
+
+    console.log('db connected');
 });
 
 
